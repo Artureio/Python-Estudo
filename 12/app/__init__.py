e88a4b1149940d24d52bf343956b97a09c3f906c
Artur Henrique <artureio2@hotmail.com>
2021-01-31 19:41:44 -0300

    exclusao

--- 12/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pessoa import Pessoa
-from .cliente import Cliente
-from .compra import Compra
-from .vendedor import Vendedor
-
-__all__ = ['Cliente', 'Compra', 'Vendedor']
--- 12/app/compra.py
+++
@@ -1,5 +0,0 @@
-class Compra(object):
-    def __init__(self, vendedor, data, valor):
-        self.vendedor = vendedor
-        self.data = data
-        self.valor = valor
