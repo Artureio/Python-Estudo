899c403bff65d503f794694aee5b400ad9a6e421
Artur Henrique <artureio2@hotmail.com>
2021-02-10 14:56:16 -0300

    intro funcoes lambda

---
+++ 13.Programação funcional/funcoes_lambda.py
@@ -0,0 +1,13 @@
+compras = (
+    {'quantidade': 2, 'valor':20},
+    {'quantidade': 3, 'valor':30},
+    {'quantidade': 40, 'valor': 2}
+)
+
+totais = tuple(
+    map(
+        lambda compra: compra['quantidade'] * compra['valor'], compras
+    )
+)
+print('precos totais:', totais)
+print('Total geral: ', sum(totais))
